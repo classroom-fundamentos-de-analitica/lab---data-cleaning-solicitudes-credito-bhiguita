dc712fcb6a61ebba70f6425810b7abcda231f846
bhiguita <125924883+bhiguita@users.noreply.github.com>
2023-06-19 21:37:13 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,21 +7,26 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
-
+import re
+from datetime import datetime
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
-    df=df.drop(['Unnamed: 0'], axis=1)
-    df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]]=df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
-    df=df.replace(to_replace="(_)|(-)",value=" ",regex=True)    
-    df=df.replace(to_replace="[,$]|(\.00$)",value="",regex=True)
-    df.monto_del_credito = df.monto_del_credito.astype("int")
-    df.comuna_ciudadano = df.comuna_ciudadano.astype("float")
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,infer_datetime_format=True,errors='ignore',dayfirst=True)
-    df.fecha_de_beneficio = df.fecha_de_beneficio.dt.strftime("%Y/%m/%d")
-    df.drop_duplicates(inplace = True)
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+
+    df.dropna(axis=0, inplace=True)
+    df.monto_del_credito = df.monto_del_credito.str.strip('$')
+    df.monto_del_credito = df.monto_del_credito.str.replace(',','') 
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+    df.fecha_de_beneficio = df.fecha_de_beneficio.apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))  
+    df.comuna_ciudadano = df.comuna_ciudadano.apply(float)
+    #colString = df.select_dtypes(include=['object']).columns.tolist()
+    
+    for i in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
+        df[i] = df[i].str.lower()
+        df[i] = df[i].str.replace('_',' ')
+        df[i] = df[i].str.replace('-',' ')
+    
+    df.drop_duplicates(inplace=True)
 
     return df
